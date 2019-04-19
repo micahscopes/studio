7c2d70bf2a959d12198d3a5ce13ff0eb23c42992
Micah Fitch <fitchmicah@gmail.com>
2019-04-18 17:58:57 -0700

    rudimentary Channel indexing

--- contentcuration/search/search_indexes.py
+++ contentcuration/search/search_indexes.py
@@ -1,17 +1,19 @@
 from haystack import indexes
-from contentcuration.models import ContentNode
+from contentcuration.models import ContentNode, Channel
 from celery_haystack.indexes import CelerySearchIndex
 import fields
 
+actively_indexed_models = [ContentNode, Channel]
+
 class ContentNodeIndex(CelerySearchIndex, indexes.Indexable):
     text = indexes.CharField(use_template=True, document=True)
-    title = indexes.CharField(model_attr='title')
+    title = indexes.NgramField(model_attr='title')
     channel_id = fields.ChannelIdField(model_attr='get_channel', faceted=True)
-    kind = indexes.CharField(model_attr='kind__kind', faceted=True)
+    content_kind = indexes.CharField(model_attr='kind__kind', faceted=True)
     published = indexes.BooleanField(model_attr='published', faceted=True)
+    language = indexes.MultiValueField(faceted=True)
 
     channel_editors = indexes.MultiValueField(faceted=True)
-    # channel_name = indexes.CharField()
 
     def get_model(self):
         return ContentNode
@@ -26,3 +28,51 @@ class ContentNodeIndex(CelerySearchIndex, indexes.Indexable):
             return [str(ed.id) for ed in channel.editors.all()]
         else:
             return []
+
+    def prepare_language(self, obj):
+        lang = obj.language
+        return [
+            lang.lang_code,
+            lang.lang_subcode,
+            lang.readable_name,
+            lang.native_name,
+            lang.lang_direction,
+            lang.ietf_name()
+        ] if lang else []
+
+
+class ChannelIndex(CelerySearchIndex, indexes.Indexable):
+    text = indexes.CharField(use_template=True, document=True)
+    name = indexes.NgramField(model_attr='name')
+    public = indexes.BooleanField(model_attr='public', faceted=True)
+    language = indexes.MultiValueField(faceted=True)
+
+    channel_editors = indexes.MultiValueField(faceted=True)
+    # channel_name = indexes.CharField()
+
+    def get_model(self):
+        return Channel
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.all()
+
+    def prepare_channel_editors(self, obj):
+        channel = obj
+        if channel:
+            return [str(ed.id) for ed in channel.editors.all()]
+        else:
+            return []
+
+    def prepare_language(self, obj):
+        lang = obj.language
+        return [
+            lang.lang_code,
+            lang.lang_subcode,
+            lang.readable_name,
+            lang.native_name,
+            lang.lang_direction,
+            lang.ietf_name()
+        ] if lang else []
+
+
--- contentcuration/search/urls.py
+++ contentcuration/search/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 import search.views as views
+from haystack.views import SearchView
 
 urlpatterns = [
+    url(r'^$', SearchView(), name='haystack_search'),
     url(r'^items/$', views.search_items),
     url(r'^topics/$', views.search_topics)
 ]
