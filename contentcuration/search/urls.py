f4e433db491b381712005b1f229d9591600c9dae
Micah Fitch <fitchmicah@gmail.com>
2019-05-13 18:08:48 -0500

    prototype search API with DRF

--- contentcuration/search/urls.py
+++ contentcuration/search/urls.py
@@ -1,9 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 import search.views as views
 from haystack.views import SearchView
+from rest_framework import routers
+
+from .views import ContentNodeSearchView
+
+router = routers.DefaultRouter()
+router.register("content", ContentNodeSearchView, base_name="content-node-search")
 
 urlpatterns = [
-    url(r'^$', SearchView(), name='haystack_search'),
+    url(r'', include(router.urls)),
     url(r'^items/$', views.search_items),
     url(r'^topics/$', views.search_topics)
 ]
+
--- contentcuration/search/views.py
+++ contentcuration/search/views.py
@@ -54,3 +54,41 @@ def search_topics(request):
     queryset = queryset.filter(title__icontains=search_query)
     serializer = serializers.SimplifiedContentNodeSerializer(queryset[:50], many=True)
     return Response(serializer.data)
+
+
+#
+# views.py
+#
+
+from drf_haystack.serializers import HaystackSerializer
+from drf_haystack.viewsets import HaystackViewSet
+
+from .search_indexes import ContentNode
+from .search_indexes import ContentNodeIndex
+from contentcuration.serializers import ContentNodeSerializer
+
+
+class ContentNodeResultSerializer(HaystackSerializer):
+    serialize_objects = True
+    class Meta:
+        # The `index_classes` attribute is a list of which search indexes
+        # we want to include in the search.
+        index_classes = [ContentNodeIndex]
+
+        # The `fields` contains all the fields we want to include.
+        # NOTE: Make sure you don't confuse these with model attributes. These
+        # fields belong to the search index!
+        fields = [
+            "title", "language", "content_kind", "channel_id", "pk"
+        ]
+
+
+class ContentNodeSearchView(HaystackViewSet):
+
+    # `index_models` is an optional list of which models you would like to include
+    # in the search result. You might have several models indexed, and this provides
+    # a way to filter out those of no interest for this particular view.
+    # (Translates to `SearchQuerySet().models(*index_models)` behind the scenes.
+    index_models = [ContentNode]
+
+    serializer_class = ContentNodeResultSerializer
