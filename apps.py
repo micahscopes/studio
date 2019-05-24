7652334cc481ef46d88c6724006d4939627d3a63
Micah Fitch <fitchmicah@gmail.com>
2019-05-24 14:36:31 -0700

    Revert "Squashed 'contentcuration/celery_haystack/' content from commit 59c029560"
    This reverts commit 8c0a00e67ac0442a6ed91e53d5a52703c14b019d.

--- apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class Celery_haystackConfig(AppConfig):
-        name = 'celery_haystack'
-
-        def ready(self):
-                from celery_haystack import signals
